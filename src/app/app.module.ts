b9f9c12c28a48a7824dc974e51ab931e609efd03
Tamas Fodor <ftamas.mail@gmail.com>
2019-05-27 13:48:50 +0200

    Add Ng-Zorro (#2)
    Angular version of the famous Ant Design component library.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,16 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { StoreModule, MetaReducer } from '@ngrx/store';
 import { storeFreeze } from 'ngrx-store-freeze';
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
 import { EnrichmentEntriesModule } from './enrichment-entries/enrichment-entries.module';
 import { EffectsModule } from '@ngrx/effects';
+
+registerLocaleData(en);
 
 export const metaReducers: MetaReducer<{}>[] = !environment.production
 ? [storeFreeze]
@@ -21,11 +29,16 @@ export const metaReducers: MetaReducer<{}>[] = !environment.production
     BrowserModule,
     AppRoutingModule,
     StoreModule.forRoot({}, { metaReducers }),
+    StoreDevtoolsModule.instrument(),
+    NgZorroAntdModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
     EffectsModule.forRoot([]),
     StoreDevtoolsModule.instrument(),
     EnrichmentEntriesModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
